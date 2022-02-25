499fadd6b03277d794ad6a871db7524553421fed
alanhernandezvillanueva <79774478+alanhernandezvillanueva@users.noreply.github.com>
2022-02-24 23:47:36 -0800

    added capability to save tasks to local storage per each hour

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+
 var nineAmTaskDesc = document.getElementById("9amtext");
 var tenAmTaskDesc = document.getElementById("10amtext");
 var elevenAmTaskDesc = document.getElementById("11amtext");
@@ -7,89 +8,156 @@ var twoPmTaskDesc = document.getElementById("2pmtext");
 var threePmTaskDesc = document.getElementById("3pmtext");
 var fourPmTaskDesc = document.getElementById("4pmtext");
 var fivePmTaskDesc = document.getElementById("5pmtext");
-var tasks = [];
+
+var currentDay = moment().format("ddd, MMM D ");
+var currentDayElement = document.getElementById("currentDay");
+currentDayElement.innerText = currentDay;
+
 
 
 
 document.getElementById("9am").addEventListener("click", function(event) {
     event.preventDefault();
-    console.log("Button Clicked");
+    console.log(moment);
     var nineAmInputText = nineAmTaskDesc.innerHTML;
-    if (nineAmTaskDesc = "")  {
-     return nineHourTask = {
+    if (!nineAmTaskDesc == "")  {
+      nineHourTask = {
             hour: "9 am",
             text: nineAmInputText,
         
         }
-        
+        localStorage.setItem("nineHourTask", JSON.stringify(nineHourTask));
     }
-    localStorage.setItem("nineHourTask", JSON.stringify(tasks));
+        
+    
 
 })
-
 
 document.getElementById("10am").addEventListener("click", function(event) {
     event.preventDefault();
     console.log("Button Clicked");
-    var tenAmInputText = nineAmTaskDesc.innerHTML;
-    if (!nineAmTaskDesc === "")  {
-     return Task = {
-            hour: "9 am",
+    var tenAmInputText = tenAmTaskDesc.innerHTML;
+    if (!tenAmTaskDesc == "")  {
+      tenHourTask = {
+            hour: "10 am",
             text: tenAmInputText,
         
         }
-        
+        localStorage.setItem("tenHourTask", JSON.stringify(tenHourTask));
     }
-    localStorage.setItem("tasks", JSON.stringify(tasks));
+        
+    
 
 })
+document.getElementById("11am").addEventListener("click", function(event) {
+    event.preventDefault();
+    console.log("Button Clicked");
+    var elevenAmInputText = elevenAmTaskDesc.innerHTML;
+    if (!elevenAmTaskDesc == "")  {
+      elevenHourTask = {
+            hour: "11 am",
+            text: elevenAmInputText,
+        
+        }
+        localStorage.setItem("elevenHourTask", JSON.stringify(elevenHourTask));
+    }
+        
+    
 
-// document.getElementById("10am").addEventListener("click", function(event) {
-//     event.preventDefault();
-//     console.log("Button Clicked");
-//     var hourNineText = tenAmTaskDesc.innerHTML;
-   
+})
+document.getElementById("12pm").addEventListener("click", function(event) {
+    event.preventDefault();
+    console.log("Button Clicked");
+    var twelvePmInputText = tenAmTaskDesc.innerHTML;
+    if (!tenAmTaskDesc == "")  {
+      twelveHourTask = {
+            hour: "12 pm",
+            text: twelvePmInputText,
+        
+        }
+        localStorage.setItem("twelveHourTask", JSON.stringify(twelveHourTask));
+    }
+        
+    
 
-//     {
-//         var blockTenTask = {
-//             hour: "9 am",
-//             text: hourNineText,
+})
+document.getElementById("1pm").addEventListener("click", function(event) {
+    event.preventDefault();
+    console.log("Button Clicked");
+    var onePmInputText = onePmTaskDesc.innerHTML;
+    if (!onePmTaskDesc == "")  {
+      oneHourTask = {
+            hour: "1 pm",
+            text: onePmInputText,
+        
+        }
+        localStorage.setItem("oneHourTask", JSON.stringify(oneHourTask));
+    }
+        
+    
 
-//         }
-//         saveTasks(blockTenTask);
+})
+document.getElementById("2pm").addEventListener("click", function(event) {
+    event.preventDefault();
+    console.log("Button Clicked");
+    var twoPmInputText = twoPmTaskDesc.innerHTML;
+    if (!twoPmTaskDesc == "")  {
+      twoHourTask = {
+            hour: "2 pm",
+            text: twoPmInputText,
+        
+        }
+        localStorage.setItem("twoHourTask", JSON.stringify(twoHourTask));
+    }
+        
+    
 
-//     }
-// })
-// tenAmBtn.addEventListener("click", function(event){
-// console.log("button clicked")
-// event.preventDefault();
-// });
-// elevenAmBtn.addEventListener("click", function(event){
-// console.log("button clicked")
-// event.preventDefault();
-//  });
-// twelvePmBtn.addEventListener("click", function(event){
-// console.log("button clicked")
-// event.preventDefault();
-//  });
-//  onePmBtn.addEventListener("click", function(event){
-// console.log("button clicked")
-// event.preventDefault();
-// });
-// twoPmBtn.addEventListener("click", function(event){
-// console.log("button clicked")
-// event.preventDefault();
-//  });
-// threePmBtn.addEventListener("click", function(event){
-// console.log("button clicked")
-// event.preventDefault();
-// });
-// fourPmBtn.addEventListener("click", function(event){
-// console.log("button clicked")
-// event.preventDefault();
-// });
-// fivePmBtn.addEventListener("click", function(event){
-// console.log("button clicked")
-// event.preventDefault();
-// });
+})
+document.getElementById("3pm").addEventListener("click", function(event) {
+    event.preventDefault();
+    console.log("Button Clicked");
+    var threePmInputText = threePmTaskDesc.innerHTML;
+    if (!threePmTaskDesc == "")  {
+      threeHourTask = {
+            hour: "3 pm",
+            text: threePmInputText,
+        
+        }
+        localStorage.setItem("threeHourTask", JSON.stringify(threeHourTask));
+    }
+        
+    
 
+})
+document.getElementById("4pm").addEventListener("click", function(event) {
+    event.preventDefault();
+    console.log("Button Clicked");
+    var fourPmInputText = fourPmTaskDesc.innerHTML;
+    if (!fourPmTaskDesc == "")  {
+      fourHourTask = {
+            hour: "4 pm",
+            text: fourPmInputText,
+        
+        }
+        localStorage.setItem("fourHourTask", JSON.stringify(fourHourTask));
+    }
+        
+    
+
+})
+document.getElementById("5pm").addEventListener("click", function(event) {
+    event.preventDefault();
+    console.log("Button Clicked");
+    var fivePmInputText = fivePmTaskDesc.innerHTML;
+    if (!fivePmTaskDesc == "")  {
+      fiveHourTask = {
+            hour: "5 pm",
+            text: fivePmInputText,
+        
+        }
+        localStorage.setItem("fiveHourTask", JSON.stringify(fiveHourTask));
+    }
+        
+    
+
+})
